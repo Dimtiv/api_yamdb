b6eb1e765b27a1980e170ebb531570f312461fb2
mowb5st <mowbnew@gmail.com>
2022-06-23 17:06:28 +0700

    fixing conflicts

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -32,7 +32,11 @@ class User(AbstractUser):
 
 
 class Genre(models.Model):
-
+    # name = models.CharField(max_length=256)
+    # slug = models.SlugField(max_length=50, unique=True)
+    #
+    # def __str__(self):
+    #     return self.name
     def __str__(self):
         return self.name
 
@@ -42,7 +46,11 @@ class Genre(models.Model):
 
 
 class Category(models.Model):
-
+    # name = models.CharField(max_length=256)
+    # slug = models.SlugField(max_length=50, unique=True)
+    #
+    # def __str__(self):
+    #     return self.name
     def __str__(self):
         return self.name
 
@@ -52,13 +60,30 @@ class Category(models.Model):
 
 
 class Title(models.Model):
+    # name = models.CharField()
+    # year = models.PositiveIntegerField(
+    #     validators=[MaxValueValidator(datetime.now().year)],
+    #     )
+    # description = models.CharField(blank=True, null=True)
+    # genre = models.ManyToManyField(Genre, through='GenreTitle')
+    # category = models.ForeignKey(
+    #     Category,
+    #     models.SET_NULL,
+    #     related_name='titles',
+    #
+    # )
+
     class Meta:
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
 
 class GenreTitle(models.Model):
-
+    # genre = models.ForeignKey(Genre, on_delete=models.CASCADE())
+    # title = models.ForeignKey(Title, on_delete=models.CASCADE())
+    #
+    # def __str__(self):
+    #     return f'{self.title} {self.genre}'
     def __str__(self):
         return f'{self.title} {self.genre}'
 
