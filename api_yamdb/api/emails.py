1239a912a37ad70d0468c9b79917ecd730c19beb
Волгин Павел <dioborn@ya.ru>
2022-06-30 02:35:17 +0300

    Проект YaMDb
    Правки

--- api_yamdb/api/emails.py
+++ api_yamdb/api/emails.py
@@ -3,10 +3,10 @@ from django.core.mail import EmailMessage
 
 class Util:
     @staticmethod
-    def send_email(reciever, token):
+    def send_email(reciever, confirmation_code):
         email = EmailMessage(
             subject='Yamdb',
-            body=f'Ваш код подтверждения: {token}',
+            body=f'Ваш код подтверждения: {confirmation_code}',
             to=[reciever]
         )
         email.send()
