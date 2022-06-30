d062b82f49cb5de8716ea1165173e7b57af9e583
Дмитрий Рухлов <5918121@gmail.com>
2022-06-30 19:44:18 +0300

    Update TitleSerializer

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -16,7 +16,7 @@ from .permissions import (
 )
 from .serializers import (
     SignUpSerializer, TokenSerializer, UserSerializer, CommentSerializer,
-    GenreSerializer, CategorySerializer, TitleGetSerializer, ReviewSerializer,
+    GenreSerializer, CategorySerializer, TitleSerializer, ReviewSerializer,
     TitlePostSerializer, MeUserSerializer
 )
 from .tokens import account_activation_token
@@ -162,12 +162,12 @@ class CategoryViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
 
 class TitleViewSet(ModelViewSet):
     queryset = Title.objects.all()
-    serializer_class = TitleGetSerializer
+    serializer_class = TitleSerializer
     permission_classes = [IsReadOnly | IsAdmin]
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
         if self.request.method in permissions.SAFE_METHODS:
-            return TitleGetSerializer
+            return TitleSerializer
         return TitlePostSerializer
